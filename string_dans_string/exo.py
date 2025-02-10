ef09e484e4c23dfe2b3d254c14d92a1d1fbf70dd
Christophe Schulz <christopheschulz@orange.fr>
2025-02-10 15:12:04 +0100

    string dans string ok

---
+++ string_dans_string/exo.py
@@ -0,0 +1,22 @@
+
+import sys
+
+def str_dans_str(args):
+    return args[1] in args[0]
+
+def verification_arguments(args):
+    ok = False
+    if len(args) == 2:
+        if not args[0].isdigit() and not args[1].isdigit():
+            ok = True
+    return ok
+
+def erreur():
+    print("error")
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    if verification_arguments(arguments):
+        print(str_dans_str(arguments))
+    else:
+        erreur()
